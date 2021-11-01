01f64275090c305e111a7f3832eaadb4d03c4597
galuhays20 <89930078+galuhays20@users.noreply.github.com>
2021-11-01 09:59:43 +0700

    Add files via upload

---
+++ chapter 6/praktikum 4/prak 4 no 1.py
@@ -0,0 +1,10 @@
+def isPythagoras(a, b, c):
+    if c*c ==(a*a + b*b):
+        print(True)
+    else:
+        print(False)
+
+a= int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+isPythagoras(a,b,c)
